778bc9c668a8a6bfd3ea76775606102797fdbb4f
Galo Mosquera <gangrelg@hotmail.com>
2020-09-14 23:48:31 -0500

    Plate validation

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { query, validationResult } = require('express-validator');
 
 // METHOD GET
 // JUST BECAUSE I'M NOT COMMITING ANY DATA SOMEWHERE
-router.get('/', (req, res) => {
-  res.send(`Route Working!`);
-});
+router.get(
+  '/',
+  [
+    query('plate')
+      .isString()
+      .isLength({ min: 6, max: 7 })
+      .matches('^[a-zA-Z0-9äöüÄÖÜ]*$'),
+  ],
+  (req, res) => {
+    const { plate, date, time } = req.query;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    return res.send('yeah');
+  }
+);
 
 module.exports = router;
