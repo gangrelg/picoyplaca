af5f5bb4ea8ec6250e7eed2c7c4358cdc86beaf1
Galo Mosquera <gangrelg@hotmail.com>
2020-09-16 15:22:52 -0500

    Fixed some deployment parameters

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const _index = require('./routes/index');
 
 const port = process.env.PORT || 5000;
 
 app.use('/api/placa', _index);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+app.use(express.static('./client/build'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'client/build/' });
 });
 
 const server = app.listen(port, () => {
