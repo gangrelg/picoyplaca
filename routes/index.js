554c1cbafc3006be8cebc22484efc81a1574b481
Galo Mosquera <gangrelg@hotmail.com>
2020-09-16 00:14:05 -0500

    Logic Implemented
    Included some tests

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { query, validationResult } = require('express-validator');
+const moment = require('moment');
+const restrictions = require('../restrictions.json');
 
 // METHOD GET
 // JUST BECAUSE I'M NOT COMMITING ANY DATA SOMEWHERE
@@ -15,16 +17,64 @@ router.get(
     query('time').isString().matches('^$|^(([01][0-9])|(2[0-3])):[0-5][0-9]$'),
   ],
   (req, res) => {
+    // ERROR VALIDATION FROM REQUEST
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { plate, date, time } = req.query;
-    const digit = plate.substr(-1);
-    const day = new Date(date).getDay();
+    try {
+      const { plate, date, time } = req.query;
+      const digit = Number(plate.substr(-1));
 
-    return res.status(200).send(`plate: ${day}`);
+      // SOME EXTRA VALIDATION THAT WE MIGHT NEED
+      if (isNaN(digit)) {
+        return res.status(500).json({ msg: 'Server error' });
+      }
+
+      if (!moment(date).isValid()) {
+        return res.status(500).json({ msg: 'Server error' });
+      }
+
+      // REQUEST QUERY STRINGS FORMAT
+      const day = new Date(date).getDay();
+
+      const format = 'hh:mm';
+
+      const { days, hours } = restrictions;
+
+      const restrictedDigitOne = days[day][0];
+      const restrictedDigitTwo = days[day][1];
+
+      // PLATE VALIDATION
+      if (digit != restrictedDigitOne && digit != restrictedDigitTwo) {
+        return res.status(200).json({ msg: 1 });
+      } else {
+        let hourSplitROne = hours[0].split('-');
+        let hourSplitRTwo = hours[1].split('-');
+        let minHourROne = hourSplitROne[0];
+        let maxHourROne = hourSplitROne[1];
+        let minHourRTwo = hourSplitRTwo[0];
+        let maxHourRTwo = hourSplitRTwo[1];
+
+        let checkTime = moment(time, format);
+        let beforeTimeROne = moment(minHourROne, format);
+        let afterTimeROne = moment(maxHourROne, format);
+        let beforeTimeRTwo = moment(minHourRTwo, format);
+        let afterTimeRTwo = moment(maxHourRTwo, format);
+
+        if (
+          checkTime.isBetween(beforeTimeROne, afterTimeROne) ||
+          checkTime.isBetween(beforeTimeRTwo, afterTimeRTwo)
+        ) {
+          return res.status(200).json({ msg: 1 });
+        } else {
+          return res.status(200).json({ msg: 0 });
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 );
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -15,9 +15,38 @@ describe('/api/placa', () => {
   describe('GET /', () => {
     it('should return a 200', async () => {
       const res = await request(server).get(
-        '/api/placa/find/?plate=pbd4180&date=01/01/2020&time=08:08'
+        '/api/placa/find/?plate=pbd4155&date=01/01/2020&time=08:08'
       );
       await expect(res.status).toBe(200);
+    });
+  });
+
+  describe('GET /', () => {
+    it('should return 1', async () => {
+      const obj = { msg: 1 };
+      const res = await request(server).get(
+        '/api/placa/find/?plate=pbd4155&date=09/16/2020&time=08:08'
+      );
+      await expect(res.body).toMatchObject(obj);
+    });
+  });
+
+  describe('GET /', () => {
+    it('should return 0', async () => {
+      const obj = { msg: 0 };
+      const res = await request(server).get(
+        '/api/placa/find/?plate=pbd4155&date=09/16/2020&time=06:00'
+      );
+      await expect(res.body).toMatchObject(obj);
+    });
+  });
+
+  describe('GET /', () => {
+    it('should return 500 server error', async () => {
+      const res = await request(server).get(
+        '/api/placa/find/?plate=pbd415a&date=09/16/2020&time=06:00'
+      );
+      await expect(res.status).toBe(500);
     });
   });
 });
